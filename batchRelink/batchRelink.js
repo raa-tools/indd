33bd087700c9a907edf7bf129ebddb6b2898571d
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-13 18:38:58 -0500

    log files that weren't able to be replaced

--- batchRelink/batchRelink.js
+++ batchRelink/batchRelink.js
@@ -8,7 +8,6 @@ Currently file names are G####.ext & extension is set to ".tif"
 
 var lib = (File($.fileName)).parent.parent + "/functionLib/";
 $.evalFile(lib + "getExtension.js");
-// $.evalFile(lib + "checkExtension.js");
 $.evalFile(lib + "getNameFromPath.js");
 
 var panelFolder = Folder.selectDialog("Pink panel folder");
@@ -17,22 +16,39 @@ var relinkFolder = Folder.selectDialog("Pick new link folder");
 var panelFiles = panelFolder.getFiles();
 
 var ext = ".tif";
+var badPanels = [];
 
 for(var j = 0; j < panelFiles.length; j++) {
     var fileName = getNameFromPath(panelFiles[j]);
     
     if(getExtension(fileName) === ".indd") {
         var doc = app.open(panelFiles[j]);
-        
+        var doc = app.activeDocument;
+        var badImages = [];
+
         for(var i = 0; i < doc.links.length; i++) {
             var gNum = doc.links[i].name.slice(0,5);
             // var ext = getExtension(doc.links[i].name);
-        
-            doc.links[i].relink(new File(relinkFolder + "/" + gNum + ext));
+            
+            try {
+                var newLink = new File(relinkFolder + "/" + gNum + ext)
+                doc.links[i].relink(newLink);
+
+            } catch (error) {
+                var badDoc = doc.name.split(".")[0];
+                badImages.push(gNum);
+            }
         }
+        
+        badPanels.push(badDoc + ": " + badImages);
         
         doc.save();
         doc.close();
     }
 
 }
+
+for(var z = 0; z < badPanels.length; z++) {
+    $.writeln(badPanels[z] + "\n");
+}
+
