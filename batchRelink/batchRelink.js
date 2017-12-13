5b160985656e3f2e2760c5ea3ccf0ac5437109ea
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-13 18:06:16 -0500

    working version

--- batchRelink/batchRelink.js
+++ batchRelink/batchRelink.js
@@ -2,13 +2,13 @@
 
 /*
 Relinks all document link to a new folder
-Currently extension is set to ".tif"
+Currently file names are G####.ext & extension is set to ".tif"
 */
 
 
 var lib = (File($.fileName)).parent.parent + "/functionLib/";
 $.evalFile(lib + "getExtension.js");
-$.evalFile(lib + "checkExtension.js");
+// $.evalFile(lib + "checkExtension.js");
 $.evalFile(lib + "getNameFromPath.js");
 
 var panelFolder = Folder.selectDialog("Pink panel folder");
@@ -21,17 +21,18 @@ var ext = ".tif";
 for(var j = 0; j < panelFiles.length; j++) {
     var fileName = getNameFromPath(panelFiles[j]);
     
-    $.writeln(checkExtension(fileName, ".indd"));
-    // if(checkExtension(panelFiles[j], ".indd")) {
-    //     var doc = app.open(panelFiles[j]);
+    if(getExtension(fileName) === ".indd") {
+        var doc = app.open(panelFiles[j]);
         
-    //     for(var i = 0; i < doc.links.length; i++) {
-    //         var gNum = doc.links[i].name.slice(0,5);
-    //         // var ext = getExtension(doc.links[i].name);
+        for(var i = 0; i < doc.links.length; i++) {
+            var gNum = doc.links[i].name.slice(0,5);
+            // var ext = getExtension(doc.links[i].name);
         
-    //         doc.links[i].relink(new File(relinkFolder + "/" + gNum + ext));
-    //         $.writeln(decodeURI(relinkFolder) + "/" + gNum + ext);
-    //     }
+            doc.links[i].relink(new File(relinkFolder + "/" + gNum + ext));
+        }
+        
+        doc.save();
+        doc.close();
+    }
 
-    // }
 }
