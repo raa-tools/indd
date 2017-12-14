9aecf69117a8910e6432a4e5a37a5bc92ee8e750
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-14 18:36:13 -0500

    use try/catch to skip missing EX##

--- NVMM-batchPlace/placeConsecutivePDFs.js
+++ NVMM-batchPlace/placeConsecutivePDFs.js
@@ -3,34 +3,45 @@
 
 #target "InDesign-8.0" // Targetting Indd CS6
 
+var lib = (File($.fileName)).parent.parent + "/_lib/";
+$.evalFile(lib + "zFill.js");
+
 var doc = app.activeDocument;
 
-for(var i = 0; i < 16; i ++){    
-    var exNum = i + 2;
-    if(exNum != 6 && exNum != 7 && exNum != 10 && exNum != 13 && exNum != 14){ //EX06 & EX07 don't have intro panels; EX10 has A & B; EX13 & EX14 don't exist
-        var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
-        dupePages(0, 2, newPDF);
+for(var i = 2; i < 16; i ++){    
 
-    } else if (exNum == 10){
-        var repeater = 0;
-        exNum = "10A";
+    try {
+        var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/_PRODUCTION_BATCHES/BATCH 1/TL Dimensional Dates/_PDFs/TL_EX" + zFill(i, 2) + "_GP01c.pdf");
+        dupePages(1, newPDF);
 
-        while(repeater < 2){
-            var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
-            dupePages(0, 2, newPDF);
+    } catch (error) {}
+
+    // if(exNum != 6 && exNum != 7 && exNum != 10 && exNum != 13 && exNum != 14){ //EX06 & EX07 don't have intro panels; EX10 has A & B; EX13 & EX14 don't exist
+        // var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
+        // dupePages(0, 2, newPDF);
+
+    // } else if (exNum == 10){
+    //     var repeater = 0;
+    //     exNum = "10A";
+
+    //     while(repeater < 2){
+    //         var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
+    //         dupePages(0, 2, newPDF);
             
-            repeater ++;
-            exNum = "10B"
-        }
-    }
+    //         repeater ++;
+    //         exNum = "10B"
+    //     }
+    // }
 }
 
-function dupePages(pageCount, pageTotal, pdfToPlace){
-    while(pageCount < pageTotal) {
-        var dupePage = doc.pages[-2].duplicate();
+function dupePages(pageTotal, pdfToPlace){
+    var dupeCount = 0;
+
+    while(dupeCount < pageTotal) {
+        var dupePage = doc.pages[-1].duplicate();
         var rects = doc.pages[-1].rectangles;
         replacePDF(rects, pdfToPlace);
-        pageCount ++;
+        dupeCount ++;
     }
 }
 
@@ -45,17 +56,4 @@ function replacePDF(frames, pdfToPlace){
         
         frames[j].place(pdfToPlace);
     }
-}
-
-function zFill(number, digits) {
-    number = number + "";
-    if(number.length < digits) {
-        var zeroes = "0";
-        
-        while(zeroes.length < digits-number.length) {
-            zeroes += "0";
-        }
-        number = zeroes + number;
-    }
-    return number
 }
