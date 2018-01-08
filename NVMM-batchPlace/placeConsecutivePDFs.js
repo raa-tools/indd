a0fc0de42de10f7c424f557bb0b28b65166d8017
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-08 14:30:28 -0500

    generalize & streamline file getter; add simple dialog

--- NVMM-batchPlace/placeConsecutivePDFs.js
+++ NVMM-batchPlace/placeConsecutivePDFs.js
@@ -1,59 +1,68 @@
-﻿// Script to duplicate last 2 pages & replace PDFs with consecutive EX##
-// Currently only for NVMM TH Production Transmittal
+﻿// Script to duplicate pages & replace PDFs with consecutive files
+
 
 #target "InDesign-8.0" // Targetting Indd CS6
 
-var lib = (File($.fileName)).parent.parent + "/_lib/";
-$.evalFile(lib + "zFill.js");
+try{
+    var doc = app.activeDocument;
 
-var doc = app.activeDocument;
-
-for(var i = 2; i < 16; i ++){    
-
-    try {
-        var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/_PRODUCTION_BATCHES/BATCH 1/TL Dimensional Dates/_PDFs/TL_EX" + zFill(i, 2) + "_GP01c.pdf");
-        dupePages(1, newPDF);
-
-    } catch (error) {}
-
-    // if(exNum != 6 && exNum != 7 && exNum != 10 && exNum != 13 && exNum != 14){ //EX06 & EX07 don't have intro panels; EX10 has A & B; EX13 & EX14 don't exist
-        // var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
-        // dupePages(0, 2, newPDF);
-
-    // } else if (exNum == 10){
-    //     var repeater = 0;
-    //     exNum = "10A";
-
-    //     while(repeater < 2){
-    //         var newPDF = new File("/Volumes/3Projects/OVMM-OhioVetMem/04_GRAPHICS/01_GRAPHIC STUDIES/06_Production Design/02 Alcoves/_PDFs/TH_EX" + zFill(exNum, 2) + "_GP01.pdf");
-    //         dupePages(0, 2, newPDF);
-            
-    //         repeater ++;
-    //         exNum = "10B"
-    //     }
-    // }
+} catch (error) {
+    alert("No document open!")
 }
 
-function dupePages(pageTotal, pdfToPlace){
-    var dupeCount = 0;
+// Dialog setup
+var pageDupeDialog = app.dialogs.add({name:"Number of pages to duplicate"});
 
-    while(dupeCount < pageTotal) {
-        var dupePage = doc.pages[-1].duplicate();
-        var rects = doc.pages[-1].rectangles;
-        replacePDF(rects, pdfToPlace);
-        dupeCount ++;
-    }
+with(pageDupeDialog.dialogColumns.add()) {
+    var pageDupeEditText = textEditboxes.add({editContents: "1", minWidth:100});
 }
 
-function replacePDF(frames, pdfToPlace){
-    for(var j = 0; j < frames.length; j++) {
-        if(frames[j].label === "slug"){
-            app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_MEDIA;
+var showDialog = pageDupeDialog.show();
 
-        } else {
-            app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_ART;
+if(showDialog) {
+    dupePages(pageDupeEditText.editContents);
+}
+
+pageDupeDialog.destroy();
+
+// Main function below
+function dupePages(pageToDupe){
+    var pdfFolder = Folder.selectDialog("Select PDF Folder");
+    var pdfFiles = pdfFolder.getFiles("*.pdf");
+    
+    for(var i = 1; i < pdfFiles.length; i ++){    
+    
+        try {
+            var newPDF = pdfFiles[i];
+            dupePages(pageToDupe, newPDF);
+    
+        } catch (error) {
+            alert(error);
         }
-        
-        frames[j].place(pdfToPlace);
+    }
+    
+    // Helper functions below
+    function dupePages(pageTotal, pdfToPlace){
+        var dupeCount = 0;
+    
+        while(dupeCount < pageTotal) {
+            var dupePage = doc.pages[-pageTotal].duplicate();
+            var rects = doc.pages[-1].rectangles;
+            replacePDF(rects, pdfToPlace);
+            dupeCount ++;
+        }
+    }
+    
+    function replacePDF(frames, pdfToPlace){
+        for(var j = 0; j < frames.length; j++) {
+            if(frames[j].label === "slug"){
+                app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_MEDIA;
+    
+            } else {
+                app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_ART;
+            }
+            
+            frames[j].place(pdfToPlace);
+        }
     }
 }
