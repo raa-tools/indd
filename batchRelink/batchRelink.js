35bce58571aca3d54d436e21fe24e5a5ada4ed7d
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-09 11:09:08 -0500

    remove lib functions & use getFiles("*.indd") instead

--- batchRelink/batchRelink.js
+++ batchRelink/batchRelink.js
@@ -2,49 +2,40 @@
 
 /*
 Relinks all document link to a new folder
-Currently file names are G####.ext & extension is set to ".tif"
+Currently file names are G####.ext & extension is set to ".tiff"
 */
-
-
-var lib = (File($.fileName)).parent.parent + "/_lib/";
-$.evalFile(lib + "getExtension.js");
-$.evalFile(lib + "getNameFromPath.js");
 
 var panelFolder = Folder.selectDialog("Pink panel folder");
 var relinkFolder = Folder.selectDialog("Pick new link folder");
 
-var panelFiles = panelFolder.getFiles();
+var panelFiles = panelFolder.getFiles("*.indd");
 
-var ext = ".tif";
+var ext = ".tiff";
 var badPanels = [];
 
 for(var j = 0; j < panelFiles.length; j++) {
-    var fileName = getNameFromPath(panelFiles[j]);
-    
-    if(getExtension(fileName) === ".indd") {
-        var doc = app.open(panelFiles[j], false);
-        // var doc = app.activeDocument;
-        var badImages = [];
+    var doc = app.open(panelFiles[j], false);
+    // var doc = app.activeDocument;
+    var badImages = [];
 
-        for(var i = 0; i < doc.links.length; i++) {
-            var gNum = doc.links[i].name.slice(0, 5);
-            // var ext = getExtension(doc.links[i].name);
-            
-            try {
-                var newLink = new File(relinkFolder + "/" + gNum + ext)
-                doc.links[i].relink(newLink);
+    for(var i = 0; i < doc.links.length; i++) {
+        var gNum = doc.links[i].name.slice(0, 5);
+        // var ext = getExtension(doc.links[i].name);
+        
+        try {
+            var newLink = new File(relinkFolder + "/" + gNum + ext)
+            doc.links[i].relink(newLink);
 
-            } catch (error) {
-                var badDoc = doc.name.split(".")[0];
-                badImages.push(gNum);
-            }
+        } catch (error) {
+            var badDoc = doc.name.split(".")[0];
+            badImages.push(gNum);
         }
-        
-        badPanels.push(badDoc + ": " + badImages);
-        
-        doc.save();
-        doc.close();
     }
+    
+    badPanels.push(badDoc + ": " + badImages);
+    
+    doc.save();
+    doc.close();
 
 }
 
