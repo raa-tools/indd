263803add5d98907a6539d1b37c7352be049061a
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-18 17:02:18 -0500

    specify layer for gNumFrame

--- showImageName/showImageName.js
+++ showImageName/showImageName.js
@@ -6,13 +6,15 @@ var doc = app.activeDocument;
 
 var varImgName = doc.textVariables.item("Image Name");
 
-var rects = doc.layers.item("IMAGES").rectangles;
+var imageLayer = doc.layers.item("IMAGES");
+var rects = imageLayer.rectangles;
 
-for(var i = 0; i < rects.length; i++) {
+for(var i = 0; i < rects.length; i++) {    
     var imgY = rects[i].geometricBounds[0];
     var imgX = rects[i].geometricBounds[1];
 
     var gNumFrame = doc.textFrames.add({geometricBounds: [imgY, imgX, imgY + 38, imgX + 512]});
+    gNumFrame.itemLayer = imageLayer;
     gNumFrame.textVariableInstances.add({associatedTextVariable: varImgName});
     gNumFrame.parentStory.appliedParagraphStyle = "New Code Bold";
     gNumFrame.parentStory.appliedCharacterStyle = doc.characterStyles.item("[None]");
