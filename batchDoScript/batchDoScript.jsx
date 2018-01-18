86ceeeb7e28afbd2962d2cb38c5b91924641013a
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-18 18:50:38 -0500

    no real change

--- batchDoScript/batchDoScript.jsx
+++ batchDoScript/batchDoScript.jsx
@@ -1,14 +1,1 @@
-﻿#target "InDesign-8.0"
-
-var scriptToRun = new File("/Users/jesentanadi/Dropbox/1-Type/z-Scripts/_JT/3-inddScripts/showImageName/showImageName.js");
-
-var panelFolder = Folder.selectDialog("Pick panel folder");
-var panelFiles = panelFolder.getFiles("*.indd");
-
-for(var fileIndex = 0; fileIndex < panelFiles.length; fileIndex++) {
-    var batchDoc = app.open(panelFiles[fileIndex]);
-    app.doScript(scriptToRun, ScriptLanguage.JAVASCRIPT);
-
-    batchDoc.save();
-    batchDoc.close();
-}
+﻿#target "InDesign-8.0"var scriptToRun = new File("/Users/jesentanadi/Dropbox/1-Type/z-Scripts/_JT/3-inddScripts/showImageName/showImageName.js");var panelFolder = Folder.selectDialog("Pick panel folder");var panelFiles = panelFolder.getFiles("*.indd");for(var fileIndex = 0; fileIndex < panelFiles.length; fileIndex++) {    var batchDoc = app.open(panelFiles[fileIndex]);    app.doScript(scriptToRun, ScriptLanguage.JAVASCRIPT);    batchDoc.save();    batchDoc.close();}
