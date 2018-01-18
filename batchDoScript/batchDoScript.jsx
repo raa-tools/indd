4efd4875559196b3eb446126a9ccb9764304a891
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-18 17:01:54 -0500

    change var names: doc to batchDoc, i to fileIndex to prevent clash in doScript()

--- batchDoScript/batchDoScript.jsx
+++ batchDoScript/batchDoScript.jsx
@@ -1,14 +1,14 @@
 ﻿#target "InDesign-8.0"
 
-var scriptToRun = new File("/Users/jesentanadi/Library/Preferences/Adobe InDesign/Version 8.0/en_US/Scripts/Scripts Panel/batch FnR/batch-find-and-replace.jsx");
+var scriptToRun = new File("/Users/jesentanadi/Dropbox/1-Type/z-Scripts/_JT/3-inddScripts/showImageName/showImageName.js");
 
 var panelFolder = Folder.selectDialog("Pick panel folder");
 var panelFiles = panelFolder.getFiles("*.indd");
 
-for(var i = 0; i < panelFiles.length; i++) {
-    var doc = app.open(panelFiles[i]);
+for(var fileIndex = 0; fileIndex < panelFiles.length; fileIndex++) {
+    var batchDoc = app.open(panelFiles[fileIndex]);
     app.doScript(scriptToRun, ScriptLanguage.JAVASCRIPT);
 
-    doc.save();
-    doc.close();
+    batchDoc.save();
+    batchDoc.close();
 }
