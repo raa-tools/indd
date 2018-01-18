d1a968ca83be86754f841e0f59f45f913bc44c79
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-18 15:35:17 -0500

    initial commit

---
+++ batchImageName/showImageName.js
@@ -0,0 +1,21 @@
+﻿#target "InDesign"
+
+app.scriptPreferences.measurementUnit = MeasurementUnits.points;
+
+var doc = app.activeDocument;
+
+var varImgName = doc.textVariables.item("Image Name");
+
+var rects = doc.layers.item("IMAGES").rectangles;
+
+for(var i = 0; i < rects.length; i++) {
+    var imgY = rects[i].geometricBounds[0];
+    var imgX = rects[i].geometricBounds[1];
+
+    var gNumFrame = doc.textFrames.add({geometricBounds: [imgY, imgX, imgY + 38, imgX + 512]});
+    gNumFrame.textVariableInstances.add({associatedTextVariable: varImgName});
+    gNumFrame.parentStory.appliedParagraphStyle = "New Code Bold";
+    gNumFrame.parentStory.appliedCharacterStyle = doc.characterStyles.item("[None]");
+    gNumFrame.parentStory.clearOverrides();
+}
+
