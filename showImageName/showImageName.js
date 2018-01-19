9628666b9c8da5c319bbb60f6f76e75ea6739c85
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-19 12:07:44 -0500

    loop through all the pages

--- showImageName/showImageName.js
+++ showImageName/showImageName.js
@@ -1,9 +1,12 @@
-﻿#target "InDesign"
+﻿#target "InDesign-8.0"
 
 app.scriptPreferences.measurementUnit = MeasurementUnits.points;
 
 var doc = app.activeDocument;
+var pages = doc.pages;
 
+var gNumLayer; var gNumFrame;
+var imageLayer = doc.layers.item("IMAGES");
 var varImgName = doc.textVariables.item("Image Name");
 
 // This needs to be fixed:
@@ -15,20 +18,25 @@ var varImgName = doc.textVariables.item("Image Name");
 //     varImgName.name = "Image Name";
 // }
 
-var gNumLayer; var gNumFrame;
-var imageLayer = doc.layers.item("IMAGES");
-var pageItems = imageLayer.allPageItems;
-
 refreshGNumLayer()
 
-for(var i = 0; i < pageItems.length; i++) {    
-    if((pageItems[i] instanceof Rectangle || pageItems[i] instanceof Oval) && pageItems[i].images.length == 1) {
-        var imgY = pageItems[i].geometricBounds[0];
-        var imgX = pageItems[i].geometricBounds[1];
-        
-        makeGNumFrame(imgY, imgX)
+for(var j = 0; j < pages.length; j++) {
+    var pageItems = pages[j].allPageItems;
+    
+    for(var i = 0; i < pageItems.length; i++) {    
+        if(pageItems[i].itemLayer === imageLayer && (pageItems[i] instanceof Rectangle || pageItems[i] instanceof Oval) && pageItems[i].images.length == 1) {
+            var imgY = pageItems[i].geometricBounds[0];
+            var imgX = pageItems[i].geometricBounds[1];
+            
+            addGNumFrame(imgY, imgX);
+        }
     }
 }
+
+
+/* ---------------- */
+/* HELPER FUNCTIONS */
+/* ---------------- */
 
 // Delete & remake layer for gNum labels
 function refreshGNumLayer() {
@@ -39,12 +47,12 @@ function refreshGNumLayer() {
     }
 
     gNumLayer = doc.layers.add({name: "G NUMBERS - DO NOT PRINT"});
-    gNumLayer.move(LocationOptions.AFTER, doc.layers[0]);
+    gNumLayer.move(LocationOptions.AFTER, doc.layers[1]);
 }
 
 // Make text frames for gNum labels
-function makeGNumFrame(yLoc, xLoc) {
-    gNumFrame = doc.textFrames.add({itemLayer: gNumLayer, geometricBounds: [yLoc, xLoc, yLoc + 38, xLoc + 512]});
+function addGNumFrame(yLoc, xLoc) {
+    gNumFrame = pages[j].textFrames.add({itemLayer: gNumLayer, geometricBounds: [yLoc, xLoc, yLoc + 38, xLoc + 512]});
     gNumFrame.textVariableInstances.add({associatedTextVariable: varImgName});
     gNumFrame.parentStory.appliedParagraphStyle = "New Code Bold";
     gNumFrame.parentStory.appliedCharacterStyle = doc.characterStyles.item("[None]");
