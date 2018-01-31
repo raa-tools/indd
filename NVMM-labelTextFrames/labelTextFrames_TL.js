1062cd0af4a2a05102e7fcfed45a3b2ff7d93b28
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-31 10:56:50 -0500

    add try/catch for error handling & dependency handling; move main part of script to main() function; clarify some docstring

--- NVMM-labelTextFrames/labelTextFrames_TL.js
+++ NVMM-labelTextFrames/labelTextFrames_TL.js
@@ -1,68 +1,86 @@
 ﻿#target "InDesign-8.0"
 
-// Library dependencies
-var lib = (File($.fileName)).parent.parent + "/_lib/";
-$.evalFile(lib + "zFill.js");
-
-var panelFolder = Folder.selectDialog("Pick Folder");
-var panelFiles = panelFolder.getFiles("*.indd");
-
-var panelTopic = "";
-
-var stTotal; var hasLeft = false; var hasRight = false;
-
-for(var i = 0; i < panelFiles.length; i++) {
-    app.scriptPreferences.measurementUnit = MeasurementUnits.points;
+try {
+    // Library dependencies
+    var lib = (File($.fileName)).parent.parent + "/_lib/";
+    $.evalFile(lib + "zFill.js");
     
-    var doc = app.open(panelFiles[i], false);
-
-    var panel = {
-        exhibit : doc.name.split(".")[0].split("_")[0],
-        topic   : doc.name.split(".")[0].split("_")[1],
-        panel   : doc.name.split(".")[0].split("_")[2]
-    };
-
-    // Excluding some panels that aren't full TL panels
-    if(panel.panel === "GP01c" && panel.panel.slice(-1)[0] === "b") {
-        doc.close();
-        continue
-    } 
+    var panelFolder = Folder.selectDialog("Pick Folder");
+    var panelFiles = panelFolder.getFiles("*.indd");
     
-    // Check if panel is within the same panel topic
-    // If not, it's because we're at the beginning of a topic, 
-    // so reset counter and flags & remember the topic we're at.
-    if(panelTopic !== panel.topic) {
-        stTotal = 0;
-        panelTopic = panel.topic;
+    var panelTopic = "";
+    var stTotal; var hasLeft = false; var hasRight = false;
+    
+    main();
+    
+} catch (error) {
+    if (error.name === "IOError") {
+        alert("zFill function missing from /_lib/");
+    
+    } else if(error.name === "TypeError") {
+        alert("No panel folder selected");
+
+    } else {
+        $.writeln(error);
     }
-
-    // Reset flags and counters per file
-    hasLeft = false; hasRight = false;
-    numOfSTOnThisPanel = 0;
-    
-    var textFrames = doc.textFrames;
-
-    // Count
-    for(var z = 0; z < textFrames.length; z++) {
-        countNumOfST(textFrames[z]);
-        
-        if (hasLeft && hasRight){
-            numOfSTOnThisPanel = 2;
-
-        } else if (hasLeft || hasRight) {
-            numOfSTOnThisPanel = 1;
-        }
-    }
-
-    // Label
-    for(var j = 0; j < textFrames.length; j++) {
-        textFrames[j].label = getLabel(textFrames[j], numOfSTOnThisPanel, stTotal);
-    }
-
-    doc.save();
-    doc.close();
-    
 }
+
+function main() {
+    for(var i = 0; i < panelFiles.length; i++) {
+        app.scriptPreferences.measurementUnit = MeasurementUnits.points;
+        
+        var doc = app.open(panelFiles[i], false);
+    
+        var panel = {
+            exhibit : doc.name.split(".")[0].split("_")[0],
+            topic   : doc.name.split(".")[0].split("_")[1],
+            panel   : doc.name.split(".")[0].split("_")[2]
+        };
+    
+        // Excluding some panels that aren't full TL panels
+        if(panel.panel === "GP01c" && panel.panel.slice(-1)[0] === "b") {
+            doc.close();
+            continue
+        } 
+        
+        // Check if panel is within the same panel topic
+        // If not, it's because we're at the beginning of a topic, 
+        // so reset counter and flags & remember the topic we're at
+        if(panelTopic !== panel.topic) {
+            stTotal = 0;
+            panelTopic = panel.topic;
+        }
+    
+        // Reset flags and counters per file
+        hasLeft = false; hasRight = false;
+        numOfSTOnThisPanel = 0;
+        
+        var textFrames = doc.textFrames;
+    
+        // Count
+        // Has to be separated because the script has to
+        // "understand" the panel before applying labels
+        for(var z = 0; z < textFrames.length; z++) {
+            countNumOfST(textFrames[z]);
+            
+            if (hasLeft && hasRight){
+                numOfSTOnThisPanel = 2;
+    
+            } else if (hasLeft || hasRight) {
+                numOfSTOnThisPanel = 1;
+            }
+        }
+    
+        // Label
+        for(var j = 0; j < textFrames.length; j++) {
+            textFrames[j].label = getLabel(textFrames[j], numOfSTOnThisPanel, stTotal);
+        }
+    
+        doc.save();
+        doc.close();
+    }
+}
+
 
 function countNumOfST(textFrame) {
     var frameX = Math.round(textFrame.geometricBounds[1]);
@@ -70,7 +88,8 @@ function countNumOfST(textFrame) {
     var objectStyle = textFrame.appliedObjectStyle.name;
 
     // Checks how many STs there are through the position of ST titles
-    // (there's always a title per ST). 1674pt is the left-most x-value for a "right" ST box.
+    // (there's always 1 title per ST)
+    // 1674pt is the left-most x-value for a "right" ST box
     if(frameX < 1674 && frameY === 2709 && objectStyle.indexOf("National") !== -1) {
         stTotal++;
         hasLeft = true;
