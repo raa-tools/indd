fe30707b43d436e8563a5443a09cf35421cd10a2
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-02-01 15:09:06 -0500

    use openDialog to select script file, with a catch; move everything else into main() function

--- batchDoScript/batchDoScript.jsx
+++ batchDoScript/batchDoScript.jsx
@@ -1,1 +1,23 @@
-﻿#target "InDesign-8.0"var scriptToRun = new File("/Users/jesentanadi/Dropbox/3-Scripts/RAATools/1-inddScripts/showImageName/showImageName.js");var panelFolder = Folder.selectDialog("Pick panel folder");var panelFiles = panelFolder.getFiles("*.indd");for(var fileIndex = 0; fileIndex < panelFiles.length; fileIndex++) {    var batchDoc = app.open(panelFiles[fileIndex]);    app.doScript(scriptToRun, ScriptLanguage.JAVASCRIPT);    batchDoc.save();    batchDoc.close();}
+﻿#target "InDesign-8.0"
+
+var scriptToRun = File.openDialog("Select script to run");
+
+if(scriptToRun.name.slice(".")[1] !== ("js" || "jsx")) {
+    alert("Can't run that here");
+
+} else {
+    main();    
+}
+
+function main() {
+    var panelFolder = Folder.selectDialog("Pick panel folder");
+    var panelFiles = panelFolder.getFiles("*.indd");
+    
+    for(var fileIndex = 0; fileIndex < panelFiles.length; fileIndex++) {
+        var batchDoc = app.open(panelFiles[fileIndex]);
+        app.doScript(scriptToRun, ScriptLanguage.JAVASCRIPT);
+    
+        batchDoc.save();
+        batchDoc.close();
+    }
+}
