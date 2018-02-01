8a8fffcecb64d38ff3bc80bd9e1e5e87cf8cc874
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-01-31 19:12:01 -0500

    add documentation

--- dupeRectangles/dupeRectangles.jsx
+++ dupeRectangles/dupeRectangles.jsx
@@ -4,23 +4,27 @@ Duplicate a set of selected rectangles and replace content with selected files
 
 #target "InDesign-8.0"
 
-var doc = app.activeWindow;
-var selectedObjects = doc.selection;
-var selectFiles = File.openDialog("Select files", multiSelect = true);
+var selectedObjects = app.activeWindow.selection;
+var selectedFiles = File.openDialog("Select files", multiSelect = true);
 
 for(var i = 0; i < selectedObjects.length; i++) {
     if(selectedObjects[i] instanceof Oval || selectedObjects[i] instanceof Rectangle || selectedObjects[i] instanceof Polygon) {
         var y = selectedObjects[i].geometricBounds[0];
         var xMax = selectedObjects[i].geometricBounds[3];
 
-        for(var j = 0; j < selectFiles.length; j++) {
+        // j starts at 0 so we can replace the content of the original objects
+        // If this is unnecessary, j can start at 1
+        for(var j = 0; j < selectedFiles.length; j++) {
             if(j === 0) {
                 var dupeObjects = selectedObjects[i];
             
-            } else {    
+            } else {
+                // Weird math to get the offset right?
                 dupeObjects = selectedObjects[i].duplicate(to = [xMax * j + xMax * (.10 / j), y]);
             }
             
+            // Determine how to crop PDF... not very elegant
+            // Also currently limited to PDFs — need to look into other graphic types
             if(dupeObjects.label === "main") {
                 app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_ART;
                 
@@ -28,7 +32,7 @@ for(var i = 0; i < selectedObjects.length; i++) {
                 app.pdfPlacePreferences.pdfCrop = PDFCrop.CROP_MEDIA;
             }
             
-            dupeObjects.place(selectFiles[j]);
+            dupeObjects.place(selectedFiles[j]);
             dupeObjects.select(existingSelection = SelectionOptions.ADD_TO);
         }
     }
