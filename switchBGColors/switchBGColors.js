a9bb66632ac5eb6a1f85fb16c538181fd8f15547
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-03-23 11:17:54 -0400

    add bg light color

--- switchBGColors/switchBGColors.js
+++ switchBGColors/switchBGColors.js
@@ -6,7 +6,7 @@ if(panelFolder === null) {
     alert("No folder selected", "Nope");
     
 } else {
-    var panelFiles = panelFolder.getFiles("*.0_gp01.indd");
+    var panelFiles = panelFolder.getFiles("*.indd");
 
     for(var i = 0; i < panelFiles.length; i++) {
       var doc = app.open(panelFiles[i], true);
@@ -18,11 +18,22 @@ if(panelFolder === null) {
         space: ColorSpace.CMYK,
         colorValue: [67, 60, 56, 39],
       };
+      
+      var newLight = doc.colors.add();
+      newLight.properties = {
+        name: "Panel BG Light", 
+        model: ColorModel.PROCESS,
+        space: ColorSpace.CMYK,
+        colorValue: [5, 2, 5, 0],
+      };
 
       var bgRectangles = doc.layers.item("BACKGROUND - DO NOT PRINT").rectangles;
 
       for(var j = 0; j < bgRectangles.length; j++) {
-        bgRectangles[j].fillColor = newBlack;
+        bgRectangles[j].fillColor = newLight;
       }
+
+      doc.save();
+      doc.close();
     }
 }
