d67ebf64a8ff87a308a2dc8edae00c58fd25fa43
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-06-21 10:41:04 -0400

    minor updates

--- NVMM-batchPlace/batchPlaceText.js
+++ NVMM-batchPlace/batchPlaceText.js
@@ -1,91 +1,1 @@
-﻿#target "InDesign-8.0"
-
-var panelFolder  = Folder.selectDialog("Pick Panel Folder");
-var scriptFolder = Folder.selectDialog("Pick Scripts Folder");
-
-if(panelFolder === null || scriptFolder === null) {
-    alert("No folder selected", "Nope");
-    
-} else {
-    var panelFiles = panelFolder.getFiles("*.indd");
-    var scriptFiles = scriptFolder.getFiles("*.txt");
-    
-    app.textImportPreferences.characterSet = TextImportCharacterSet.UTF8;
-    app.textImportPreferences.stripReturnsBetweenLines = true;
-
-    for(var j = 0; j < panelFiles.length; j ++) {
-        var doc = app.open(panelFiles[j], false);
-        
-        var panel = {
-            exhibit : doc.name.split("_")[0],
-            topic   : doc.name.split("_")[1],
-            panel   : doc.name.split("_")[2]
-        };
-        
-        if(panel.topic != "EX06" && panel.topic != "EX07" && panel.panel != "GP02") {
-            var scriptType = chooseScriptType(panel.panel);
-
-            var titleFile = panel.exhibit + "_" + panel.topic + "_" + scriptType + "-T.txt";
-            var bodyFile  = panel.exhibit + "_" + panel.topic + "_" + scriptType + "-B.txt";
-        
-            var textBoxes = doc.textFrames;
-            var objectStyle = chooseObjectStyles(panel.panel);
-            
-            for(var i = 0; i < textBoxes.length; i++) {    
-                if(textBoxes[i].appliedObjectStyle == objectStyle.title){
-                    fillTextBox(i, titleFile, objectStyle.title);
-
-                } else if (textBoxes[i].appliedObjectStyle == objectStyle.body) {
-                    fillTextBox(i, bodyFile, objectStyle.body);
-
-                } else if(textBoxes[i].appliedObjectStyle == objectStyle.section) {
-                    fillTextBox(i, titleFile, objectStyle.section);
-                }
-            }
-        
-            doc.save();
-        }
-        
-        doc.close();
-    }
-}
-
-function chooseScriptType(panelType){
-    if(panelType === "GP01") {
-        return "IP01";
-
-    } else if(panelType === "GP04") {
-        return "PT01";
-
-    } else if(panelType === "GP05"){
-        return "ST01";
-    }
-}
-
-function chooseObjectStyles(panelType){
-    if(panelType === "GP01") {
-        return {
-            section : doc.objectStyles.item("AL Intro SECTION"),
-            title   : doc.objectStyles.item("AL Intro TITLE"),
-            body    : doc.objectStyles.item("AL Intro BODY")
-        };
-    
-    } else if(panelType === "GP04") {
-        return {
-            title : doc.objectStyleGroups.item("AL Primary").objectStyles.item("AL Primary TItle"),
-            body  : doc.objectStyleGroups.item("AL Primary").objectStyles.item("AL Primary Body")
-        };
-    
-    } else if(panelType === "GP05") {
-        return {
-            title : doc.objectStyleGroups.item("AL Secondary").objectStyles.item("AL Secondary Title"),
-            body  : doc.objectStyleGroups.item("AL Secondary").objectStyles.item("AL Secondary Body")
-        };
-    }
-}
-
-function fillTextBox(index, inputFileType, objectStyle){
-    textBoxes[index].contents = "";
-    textBoxes[index].place(File(scriptFolder + "/" + inputFileType));
-    textBoxes[index].applyObjectStyle(objectStyle, true);
-}
+﻿#target "InDesign-8.0"var panelFolder  = Folder.selectDialog("Pick Panel Folder");var scriptFolder = Folder.selectDialog("Pick Scripts Folder");if(panelFolder === null || scriptFolder === null) {    alert("No folder selected", "Nope");    } else {    var panelFiles = panelFolder.getFiles("*.indd");    var scriptFiles = scriptFolder.getFiles("*.txt");        app.textImportPreferences.characterSet = TextImportCharacterSet.UTF8;    app.textImportPreferences.stripReturnsBetweenLines = true;    for(var j = 0; j < panelFiles.length; j ++) {        var doc = app.open(panelFiles[j], false);                var panel = {            exhibit : doc.name.split("_")[0],            topic   : doc.name.split("_")[1],            panel   : doc.name.split("_")[2].split(".")[0]        };                if(panel.topic != "EX06" && panel.topic != "EX07" && panel.panel != "GP02") {            var scriptType = chooseScriptType(panel.panel);            var titleFile = panel.exhibit + "_" + panel.topic + "_" + scriptType + "-T.txt";            var bodyFile  = panel.exhibit + "_" + panel.topic + "_" + scriptType + "-B.txt";                    var textBoxes = doc.textFrames;            var objectStyle = chooseObjectStyles(panel.panel);             for(var i = 0; i < textBoxes.length; i++) {                if(textBoxes[i].appliedObjectStyle == objectStyle.title){                    fillTextBox(i, titleFile, objectStyle.title);                } else if (textBoxes[i].appliedObjectStyle == objectStyle.body) {                    fillTextBox(i, bodyFile, objectStyle.body);                } else if(textBoxes[i].appliedObjectStyle == objectStyle.section) {                    fillTextBox(i, titleFile, objectStyle.section);                }            }                    doc.save();        }                doc.close();    }}function chooseScriptType(panelType){    if(panelType === "GP01") {        return "IP01";    } else if(panelType === "GP04") {        return "PT01";    } else if(panelType === "GP05"){        return "ST01";    }}function chooseObjectStyles(panelType){    if(panelType === "GP01") {        return {            section : doc.objectStyles.item("AL Intro SECTION"),            title   : doc.objectStyles.item("AL Intro TITLE"),            body    : doc.objectStyles.item("AL Intro BODY")        };        } else if(panelType === "GP04") {        return {            title : doc.objectStyleGroups.item("AL Primary").objectStyles.item("AL Primary TItle"),            body  : doc.objectStyleGroups.item("AL Primary").objectStyles.item("AL Primary Body")        };        } else if(panelType === "GP05") {        return {            title : doc.objectStyleGroups.item("AL Secondary").objectStyles.item("AL Secondary Title"),            body  : doc.objectStyleGroups.item("AL Secondary").objectStyles.item("AL Secondary Body")        };    }}function fillTextBox(index, inputFileType, objectStyle){    textBoxes[index].contents = "";    textBoxes[index].place(File(scriptFolder + "/" + inputFileType));    textBoxes[index].applyObjectStyle(objectStyle, true);}
--- sizeToFit/sizeToFit.js
+++ sizeToFit/sizeToFit.js
@@ -1,41 +1,1 @@
-#target InDesign
-
-var INCREMENT = 0.5;
-
-var getPointSize = function(textObjects) {
-  var retSize = 0;
-  for(var i = 0; i < textObjects.length; i ++) {
-    if(textObjects[i].pointSize >= retSize) {
-      retSize = textObjects[i].pointSize;
-    }
-  }
-  return retSize;
-}
-
-var checkOverflow = function(frame) {
-  return frame.overflows
-}
-
-var increasePointSize = function(textObjects, point) {
-  for(var i = 0; i < textObjects.length; i ++) {
-    currentPointSize += point;
-    textObjects[i].pointSize += point;
-  }
-}
-
-var setPointSize = function(frame, point) {
-  point -= INCREMENT;
-  frame.parentStory.pointSize = point;
-}
-
-var textframe = app.selection[0];
-var texts = textframe.texts
-var overflow = false;
-
-var currentPointSize = getPointSize(texts);
-
-while(!checkOverflow(textframe)) {
-  increasePointSize(texts, INCREMENT);
-}
-
-setPointSize(textframe, currentPointSize)
+﻿#target InDesignvar INCREMENT = 0.1;var LEADINGPERCENT = 100;var getPointSize = function(textObjects) {  var retSize = 0;  for(var i = 0; i < textObjects.length; i ++) {    if(textObjects[i].pointSize >= retSize) {      retSize = textObjects[i].pointSize;    }  }  return retSize;}var checkOverflow = function(frame) {  return frame.overflows}var increasePointSize = function(textObjects, point) {  for(var i = 0; i < textObjects.length; i ++) {    currentPointSize += point;    textObjects[i].pointSize += point;    textObjects[i].autoLeading = LEADINGPERCENT;  }}var setPointSize = function(frame, point) {  point -= INCREMENT;  frame.parentStory.pointSize = point;}var textframe = app.selection[0];var texts = textframe.textsvar overflow = false;var currentPointSize = getPointSize(texts);while(!checkOverflow(textframe)) {  increasePointSize(texts, INCREMENT);}setPointSize(textframe, currentPointSize)
