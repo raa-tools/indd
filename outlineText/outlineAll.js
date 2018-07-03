bf05f2f1fc481c49c832c2189396537f52eb34ca
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-07-03 18:46:59 -0400

    fixityfixfix

--- outlineText/outlineAll.js
+++ outlineText/outlineAll.js
@@ -1,11 +1,19 @@
-#target "InDesign-8.0"
+﻿#target "InDesign-8.0"
 
 var pages = app.activeDocument.pages
 
-for(var i = 0; i < pages.length; i ++) {
-  var textboxes = pages[i].textFrames
+var outlineText = function(item) {
+  if(item.itemLayer.name === "Code and info" || !(item instanceof TextFrame)) {
+    return
+  }
+  item.createOutlines();
+}
 
-  for(var j = 0; j < textboxes.length; j++) {
-    textboxes[j].createOutlines()
+for(var i = 0; i < pages.length; i ++) {
+  var pageItems = pages[i].allPageItems;
+
+  for(var j = 0; j < pageItems.length; j++) {
+    outlineText(pageItems[j])
   }
 }
+
