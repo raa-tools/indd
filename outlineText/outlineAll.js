4945f9b062f0df577572891661627f14e8088cf7
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-07-05 10:37:26 -0400

    outline all text, including Code and info

--- outlineText/outlineAll.js
+++ outlineText/outlineAll.js
@@ -1,19 +1,1 @@
-﻿#target "InDesign-8.0"
-
-var pages = app.activeDocument.pages
-
-var outlineText = function(item) {
-  if(item.itemLayer.name === "Code and info" || !(item instanceof TextFrame)) {
-    return
-  }
-  item.createOutlines();
-}
-
-for(var i = 0; i < pages.length; i ++) {
-  var pageItems = pages[i].allPageItems;
-
-  for(var j = 0; j < pageItems.length; j++) {
-    outlineText(pageItems[j])
-  }
-}
-
+﻿#target "InDesign-8.0"var pages = app.activeDocument.pagesvar outlineText = function(item) {  if(!(item instanceof TextFrame)) {    return  }  item.createOutlines();}for(var i = 0; i < pages.length; i ++) {  var pageItems = pages[i].allPageItems;  for(var j = 0; j < pageItems.length; j++) {    outlineText(pageItems[j])  }}
