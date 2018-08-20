2f7b3c9771a341d6f21664788178b6a24bf4d3f1
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-08-20 18:15:59 -0400

    new

---
+++ findItalics/findItalics.jsx
@@ -0,0 +1,10 @@
+﻿var doc = app.activeDocument
+var usedFonts = doc.fonts;
+
+if(!Folder(doc.filePath + "/italics").exists)  Folder(doc.filePath+"/_italics").create();
+
+for(var i = 0; i < usedFonts.length; i++) {
+    var fontWeight = usedFonts[i].name.split("	")[1];
+    
+    if(fontWeight.toLowerCase().indexOf("italic") !== -1) doc.saveACopy(File(doc.filePath + "/_italics/" +doc.name));
+}
