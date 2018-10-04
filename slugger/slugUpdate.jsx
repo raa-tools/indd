4a503601bdaef17855af33ce7bfc35d42ab218a8
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-10-04 15:57:07 -0400

    add error logging; use time as part of file name

--- slugger/slugUpdate.jsx
+++ slugger/slugUpdate.jsx
@@ -1,5 +1,7 @@
 ﻿#target InDesign
 
+var BADFILESLIST = [];
+
 try{
   // These are declared here so they can be used by dialogSetup()
   var myWindow; var batchEditText; var reviewEditText; var dateEditText;
@@ -33,6 +35,15 @@ try{
         main(panelFile);
         panelFile.save();
         panelFile.close();
+      }
+
+      if(BADFILESLIST.length !== 0) {
+        var date = new Date();
+        var now = date.getTime();
+        var logFileName = "~/Desktop/slugUpdateLog_" + now +".txt"
+
+        writeLogFile(BADFILESLIST, logFileName)
+        alert("Some of the files weren't updated\nCheck " + logFileName);
       }
     } else {
         alert("No updates made!\r Select something to update.");
@@ -133,67 +144,63 @@ function dialogSetup() {
 
 
 function main(docToUpdate) {
-        
-    var codeInfoLayer = docToUpdate.layers.item("Code and info");
+  var codeInfoLayer = docToUpdate.layers.item("Code and info");
 
-    // Log files that have missing "Code and info" layer
-    // and continue to next file in the loop
-    // (Better than breaking and losing progress)
-    if(!codeInfoLayer.isValid) {
-        try {
-            // Global variables from roadRunner
-            MISSINGLAYER = true;
-            BADFILESLIST.push(docToUpdate.name.replace(".indd", ""));
+  // Log files that have missing "Code and info" layer
+  // and continue to next file in the loop
+  // (Better than breaking and losing progress)
+  if(!codeInfoLayer.isValid) {
+    BADFILESLIST.push(docToUpdate.name.replace(".indd", ""));
 
-        } catch(error) {
-            // The block above will throw a ReferenceError because 
-            // the global variables are declared in roadRuner
-            // if(error.name === "ReferenceError") {
-            //     alert('"Code and info" layer missing');
-            // }
-        }
-        
-        
-    } else {
-        codeInfoLayer.locked = false;
-        codeInfoLayer.move(LocationOptions.BEFORE, docToUpdate.layers[0]);
-        
-        var codeInfoFrames = codeInfoLayer.textFrames;
+  } else {
+    codeInfoLayer.locked = false;
+    codeInfoLayer.move(LocationOptions.BEFORE, docToUpdate.layers[0]);
+    
+    var codeInfoFrames = codeInfoLayer.textFrames;
 
-        if(batchReviewCheck.value && dateCheck.value) {
-            for(var i = 0; i < codeInfoFrames.length; i++) {
-                updateBatchReview();
-                updateDate();
-            }
-        } else if(batchReviewCheck.value) {
-            for(var i = 0; i < codeInfoFrames.length; i++) {
-                updateBatchReview();
-            }
-        } else if(dateCheck.value) {
-            for(var i = 0; i < codeInfoFrames.length; i++) {
-                updateDate();
-            }
-        }
-
-        //Re-lock Code and info layer
-        docToUpdate.layers.item("Code and info").locked = true;
-
+    if(batchReviewCheck.value && dateCheck.value) {
+      for(var i = 0; i < codeInfoFrames.length; i++) {
+        updateBatchReview();
+        updateDate();
+      }
+    } else if(batchReviewCheck.value) {
+      for(var i = 0; i < codeInfoFrames.length; i++) {
+        updateBatchReview();
+      }
+    } else if(dateCheck.value) {
+      for(var i = 0; i < codeInfoFrames.length; i++) {
+        updateDate();
+      }
     }
 
-    function updateBatchReview(){
-        if(codeInfoFrames[i].label === "batchReviewInput") {
-            if(!fabCheck.value) {
-                codeInfoFrames[i].contents = "Batch " + batchEditText.text + " - " + "Review " + reviewEditText.text;
-                
-            } else {
-                codeInfoFrames[i].contents = "Batch " + batchEditText.text + " - " + "TO " + fabEditText.text.toUpperCase();
-            }
-        }
-    }
+    //Re-lock Code and info layer
+    docToUpdate.layers.item("Code and info").locked = true;
 
-    function updateDate(){
-        if(codeInfoFrames[i].label === "dateInput") {
-            codeInfoFrames[i].contents = dateEditText.text;
-        }
+  }
+
+  function updateBatchReview(){
+    if(codeInfoFrames[i].label === "batchReviewInput") {
+      if(!fabCheck.value) {
+        codeInfoFrames[i].contents = "Batch " + batchEditText.text + " - " + "Review " + reviewEditText.text;  
+      } else {
+        codeInfoFrames[i].contents = "Batch " + batchEditText.text + " - " + "TO " + fabEditText.text.toUpperCase();
+      }
     }
+  }
+
+  function updateDate(){
+    if(codeInfoFrames[i].label === "dateInput") {
+      codeInfoFrames[i].contents = dateEditText.text;
+    }
+  }
+}
+
+function writeLogFile(listToOutput, fileName){
+  var logFile = new File(fileName);
+  logFile.encoding = "UTF-8";
+  
+  logFile.open("w");
+  logFile.write("These panels didn't quite work out:\n");
+  logFile.write(listToOutput.join("\n"));
+  logFile.close();
 }
