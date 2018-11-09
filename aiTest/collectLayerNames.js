2b73e74e9ec8cd22c780092ace79a71a602d6871
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-09 10:33:55 -0500

    test script for JD

---
+++ aiTest/collectLayerNames.js
@@ -0,0 +1,24 @@
+﻿#target illustrator
+
+var doc = app.activeDocument;
+var layers = doc.layers;
+var layerNames = [];
+
+for(var i = 0; i < layers.length; i++) {
+  layerNames.push(layers[i].name)
+}
+
+var width = 500
+var height = 1000
+var margin = 30
+
+var textPath = doc.pathItems.add();
+textPath.setEntirePath ([
+    [margin, -margin],
+    [margin, -height - margin],
+    [width + margin, -height - margin],
+    [width + margin, -margin]
+]);
+
+var newTextFrame = doc.textFrames.areaText(textPath);
+newTextFrame.contents = layerNames.join("\n")
