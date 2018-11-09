b1fa2b8bc99fb69061664f89c7cf3bd420775b5d
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-11-09 14:53:38 -0500

    initial

---
+++ commitListener/commitListener.js
@@ -0,0 +1,22 @@
+﻿#target "InDesign-8.0"
+#targetengine "session"
+
+app.addEventListener("afterOpen", watchDocument);
+
+function watchDocument() {
+  if (!app.layoutWindows.length) return;
+  
+  var doc = app.activeDocument;
+  doc.addEventListener("afterSave", function() {
+    doc.addEventListener("beforeClose", function() {
+      askForCommitMessage(doc.name)
+    })
+  })
+}
+
+function askForCommitMessage(docName) {
+  // Currently this is happening twice per document
+  $.writeln(docName + ": closed")
+}
+
+
---
+++ warnReadOnly/warnReadOnly.jsx
@@ -0,0 +1,13 @@
+﻿#target "InDesign-8.0"
+#targetengine "session"
+
+app.addEventListener("afterOpen", watchDocument);
+
+function watchDocument() {
+  if (!app.layoutWindows.length) return;
+
+  doc = app.activeDocument
+  if(doc.readOnly) {
+      alert(doc.name + " is read only")
+  }
+}
