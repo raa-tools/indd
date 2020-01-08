282e4e2719d8b9129e05414d251181d78de33cc4
Jesen Tanadi <mail@jesentanadi.com>
2020-01-08 15:11:25 -0500

    initial commit new script

---
+++ batchResize/batchResize.js
@@ -0,0 +1,36 @@
+#target InDesign
+
+var panelFolder = Folder.selectDialog("Pick panel folder");
+var panelFiles = panelFolder.getFiles("*.indd");
+
+// Progress bar
+var w = new Window("palette");
+w.progressBar = w.add ('progressbar', undefined, 0, panelFiles.length);
+w.progressBar.preferredSize.width = 300;
+w.show();
+
+app.scriptPreferences.measurementUnit = MeasurementUnits.points;
+for(var j = 0; j < panelFiles.length; j++) {
+    var doc = app.open(panelFiles[j], false);
+    w.progressBar.value = j+1;
+
+    var page = doc.pages[0];
+
+    var width = page.bounds[3] - page.bounds[1]
+    var height = page.bounds[2] - page.bounds[0]
+
+    var newWidth;
+    if (width === 48 * 72) {
+      newWidth = 48.3 * 72
+      $.writeln("LONG", newWidth)
+    } else if (width === 24 * 72) {
+      newWidth = 23.8 * 72
+    }
+
+    var newHeight = 3.93 * 72;
+    
+    page.resize(CoordinateSpaces.INNER_COORDINATES, AnchorPoint.CENTER_ANCHOR, ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH, [newWidth, newHeight])
+
+    doc.save();
+    doc.close();
+}
