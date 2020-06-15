a8afa160004ac259520fb3c0e2c0e71aa4cb62bd
Jesen Tanadi <mail@jesentanadi.com>
2020-06-15 11:59:14 -0400

    update

--- batchResize/batchResize.js
+++ batchResize/batchResize.js
@@ -3,6 +3,13 @@
 var panelFolder = Folder.selectDialog("Pick panel folder");
 var panelFiles = panelFolder.getFiles("*.indd");
 
+var NEW_WIDTH_FULL = 44.1875
+var NEW_WIDTH_HALF = 21.75
+var NEW_HEIGHT = 3.93
+var DPI = 72
+
+var badPanels = []
+
 // Progress bar
 var w = new Window("palette");
 w.progressBar = w.add ('progressbar', undefined, 0, panelFiles.length);
@@ -20,17 +27,23 @@ for(var j = 0; j < panelFiles.length; j++) {
     var height = page.bounds[2] - page.bounds[0]
 
     var newWidth;
-    if (width === 48 * 72) {
-      newWidth = 48.3 * 72
+    if (width === 48 * DPI) {
+      newWidth = NEW_WIDTH_FULL * DPI
       $.writeln("LONG", newWidth)
-    } else if (width === 24 * 72) {
-      newWidth = 23.8 * 72
+    } else if (width === 24 * DPI) {
+      newWidth = NEW_WIDTH_HALF * DPI
     }
 
-    var newHeight = 3.93 * 72;
+    var newHeight = NEW_HEIGHT * 72;
     
-    page.resize(CoordinateSpaces.INNER_COORDINATES, AnchorPoint.CENTER_ANCHOR, ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH, [newWidth, newHeight])
+    if (newWidth && newHeight) {
+      page.resize(CoordinateSpaces.INNER_COORDINATES, AnchorPoint.CENTER_ANCHOR, ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH, [newWidth, newHeight])
+      doc.save();
+    } else {
+      badPanels.push(doc.name)
+    }
 
-    doc.save();
     doc.close();
 }
+
+$.writeln(badPanels.join("\n"))
